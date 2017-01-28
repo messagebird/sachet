36c9726a5c4288fe60b9cbba9646f6db4b63fb9c
Sarath S Pillai <sarath.sp06@gmail.com>
2017-01-28 17:05:40 +0530

    Added exotel integration usig net/http instead of requests

--- exotel.go
+++ exotel.go
@@ -1,67 +1,64 @@
-package main
+package sachet
 
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/parnurzeal/gorequest"
+	"net/url"
+	"strings"
 	"time"
 )
 
-//ExotelBaseURL is the base url for exotel api
-const ExotelBaseURL = "https://twilix.exotel.in"
-//ExotelRetryInterval is the retry interval for failed requests
-const ExotelRetryInterval = 3 //in Seconds
+//ExotelConfig configuration struct for exotel Client
+type ExotelConfig struct {
+	AccountSID string `yaml:"account_sid"`
+	AuthToken  string `yaml:"auth_token"`
+}
+
+//ExotelRequestTimeout  is the timeout for http request to exotel
+const ExotelRequestTimeout = time.Second * 20
 
 //Exotel is the exte Exotel
 type Exotel struct {
 	AccountSid string
 	Token      string
-	requester  *gorequest.SuperAgent
 }
 
 //NewExotel creates a new
-func NewExotel(sid ,token string) *Exotel {
-	Exotel := &Exotel{requester: gorequest.New().SetBasicAuth(sid, token), AccountSid: sid, Token: token}
+func NewExotel(config ExotelConfig) *Exotel {
+	Exotel := &Exotel{AccountSid: config.AccountSID, Token: config.AuthToken}
 	return Exotel
 }
 
-//SetHeaders Sets needed headers
-func (c *Exotel) setHeaders() *Exotel {
-	c.requester.
-		Set("User-Agent", "sachet-v1").
-		Set("Accept-language", "es").
-		Set("Content-Type", "application/x-www-form-urlencoded").
-		Set("accept", "application/json")
-	return c
-}
+//Send send sms to n number of people using bulk sms api
+func (c *Exotel) Send(message Message) (err error) {
+	smsURL := fmt.Sprintf("https://twilix.exotel.in/v1/Accounts/%s/Sms/send.json", c.AccountSid)
+	var request *http.Request
+	var resp *http.Response
 
-//sendSMS sends sms  ,obviously  
-//This API is rate limited to 200 SMSes per minute. Once this limit has been crossed, your requests will be rejected with an HTTP 429 'Too Many Requests' code.
-func (c *Exotel) sendSMS(from string, to []string,body string) ( err error) {
-	c.requester.Post(fmt.Sprintf("%s/v1/Accounts/%s/Sms/send.json", ExotelBaseURL, c.AccountSid)).
-		Param("From", from).
-		Param("Body", body).
-        Retry(3,time.Second * ExotelRetryInterval)
-    //adding to numbers
-    for i,number :=  range to {
-        c.requester.Param(fmt.Sprintf("To[%d]",i), number)
-    }
-	c.setHeaders()
-	resp, body, errs := c.requester.End()
-	if errs != nil && len(errs) > 0 {
-		return  fmt.Errorf("Failed making sms request using exotel ,  ERRORS : %+v", errs)
+	form := url.Values{"From": {message.From}, "Body": {message.Text}, "To": message.To}
+
+	//preparing the request
+	request, err = http.NewRequest("POST", smsURL, strings.NewReader(form.Encode()))
+	if err != nil {
+		return
+	}
+
+	request.SetBasicAuth(c.AccountSid, c.Token)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Set("User-Agent", "SachetV1.0")
+	//calling the endpoint
+	httpClient := &http.Client{}
+	httpClient.Timeout = ExotelRequestTimeout
+
+	resp, err = httpClient.Do(request)
+	if err != nil {
+		return
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusOK {
-		return nil
+	var body []byte
+	resp.Body.Read(body)
+	if resp.StatusCode == http.StatusOK && err == nil {
+		return
 	}
-	return fmt.Errorf("Failed sending sms:Reason: %s , StatusCode : %d", body, resp.StatusCode)
-}
-
-
-//Send send sms to n number of people using bulk sms api  
-func (*Exotel) Send(message Message) (err error) {
-   client := NewExotel(config.Providers.Exotel.AccountSID, config.Providers.Exotel.Token)
-   return client.sendSMS(message.From, message.To, message.Text)
+	return fmt.Errorf("Failed sending sms:Reason: %s , StatusCode : %d", string(body), resp.StatusCode)
 }
