a194de4d2ce67ed9b232354300945e9504e8c1af
root <root@my>
2019-09-23 12:59:56 +0000

    send several messages if text size exceeds 4096 characters

--- provider/telegram/telegram.go
+++ provider/telegram/telegram.go
@@ -5,6 +5,11 @@ import (
 
 	"github.com/messagebird/sachet"
 	"gopkg.in/telegram-bot-api.v4"
+	"math"
+)
+
+const (
+	messageSize = 4096 
 )
 
 type TelegramConfig struct {
@@ -14,6 +19,8 @@ type TelegramConfig struct {
 type Telegram struct {
 	bot *tgbotapi.BotAPI
 }
+
+
 
 func NewTelegram(config TelegramConfig) (*Telegram, error) {
 	bot, err := tgbotapi.NewBotAPI(config.Token)
@@ -26,16 +33,41 @@ func NewTelegram(config TelegramConfig) (*Telegram, error) {
 	}, nil
 }
 
+func splitByLength(text string, messageSize int) []string {
+    runeStr := []rune(text)
+    strLength := len(runeStr)
+    numChunks := int(math.Ceil(float64(strLength) / float64(messageSize)))
+
+    splited := make([]string, numChunks)
+
+    start,stop := 0,0
+    for i := 0; i < numChunks; i += 1 {
+        start = i * messageSize
+        stop = start + messageSize
+        if stop > strLength {
+            stop = strLength
+        }
+        splited[i] = string(runeStr[start : stop])
+    }
+    return splited
+}
+
+
 func (tg *Telegram) Send(message sachet.Message) error {
 	for _, sChatID := range message.To {
 		chatID, err := strconv.ParseInt(sChatID, 10, 64)
 		if err != nil {
 			return err
 		}
-		msg := tgbotapi.NewMessage(chatID, message.Text)
-		_, err = tg.bot.Send(msg)
-		if err != nil {
-			return err
+		
+		sendedMsg := splitByLength(message.Text, messageSize)
+
+		for _, v := range sendedMsg {
+			msg := tgbotapi.NewMessage(chatID, v)
+			_, err = tg.bot.Send(msg)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
