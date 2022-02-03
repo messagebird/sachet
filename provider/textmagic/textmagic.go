7f0c69e3e2f1be0bea8f251051d5fddad191d87f
Andrew Bate <code@andrewbate.com>
2022-02-03 00:23:33 +0000

    Run go fmt.

--- provider/textmagic/textmagic.go
+++ provider/textmagic/textmagic.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"strings"
 
-	textmagic "github.com/textmagic/textmagic-rest-go-v2/v2"
 	"github.com/messagebird/sachet"
+	textmagic "github.com/textmagic/textmagic-rest-go-v2/v2"
 )
 
 type Config struct {
@@ -16,18 +16,18 @@ type Config struct {
 }
 
 type TextMagic struct {
-	client             *textmagic.APIClient
-	auth               context.Context
+	client *textmagic.APIClient
+	auth   context.Context
 }
 
 func NewTextMagic(config Config) *TextMagic {
-    cfg := textmagic.NewConfiguration()
-    cfg.BasePath = "https://rest.textmagic.com"
-    client := textmagic.NewAPIClient(cfg)
-    auth := context.WithValue(context.Background(), textmagic.ContextBasicAuth, textmagic.BasicAuth{
-        UserName: config.Username,
-        Password: config.APIKey,
-    })
+	cfg := textmagic.NewConfiguration()
+	cfg.BasePath = "https://rest.textmagic.com"
+	client := textmagic.NewAPIClient(cfg)
+	auth := context.WithValue(context.Background(), textmagic.ContextBasicAuth, textmagic.BasicAuth{
+		UserName: config.Username,
+		Password: config.APIKey,
+	})
 	return &TextMagic{
 		client: client,
 		auth:   auth,
@@ -39,11 +39,11 @@ func (tm *TextMagic) Send(message sachet.Message) error {
 	switch message.Type {
 	case "", "text":
 		joinedPhones := strings.Join(message.To[:], ",")
-    	_, _, err = tm.client.TextMagicApi.SendMessage(tm.auth, textmagic.SendMessageInputObject{
-        	Text:   message.Text,
-    		Phones: joinedPhones,
-    		From:   message.From,
-    	})
+		_, _, err = tm.client.TextMagicApi.SendMessage(tm.auth, textmagic.SendMessageInputObject{
+			Text:   message.Text,
+			Phones: joinedPhones,
+			From:   message.From,
+		})
 	default:
 		return fmt.Errorf("unknown message type %s", message.Type)
 	}
