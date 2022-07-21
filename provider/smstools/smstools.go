f0aba81c2e1a45315c4a220861d5a441e64d68bf
Christian Pointner <equinox@spreadspace.org>
2022-07-22 01:09:01 +0200

    smstools provider: fix file permission issues

--- provider/smstools/smstools.go
+++ provider/smstools/smstools.go
@@ -1,7 +1,13 @@
 package smstools
 
 import (
-	"io/ioutil"
+	"encoding/binary"
+	"fmt"
+	"hash/fnv"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/messagebird/sachet"
 )
@@ -21,14 +27,42 @@ func NewSmsTools(config Config) *SmsTools {
 	return SmsTools
 }
 
+func (smst *SmsTools) createFile(recipient, message string) (*os.File, error) {
+	h := fnv.New32()
+	if _, err := io.WriteString(h, recipient+message); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(h, binary.LittleEndian, time.Now().UnixNano()); err != nil {
+		return nil, err
+	}
+
+	hsum := h.Sum32()
+	prefix := filepath.Join(smst.OutgoingDir, "sachet-")
+
+	try := 0
+	for {
+		name := fmt.Sprintf("%s%d", prefix, hsum)
+		f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+		if os.IsExist(err) {
+			if try++; try < 10000 {
+				hsum = hsum + 1
+				continue
+			}
+			return nil, fmt.Errorf("unable to create conflict-free filename")
+		}
+		return f, err
+	}
+}
+
 func (smst *SmsTools) Send(message sachet.Message) error {
 	for _, recipient := range message.To {
-		outfile, err := ioutil.TempFile(smst.OutgoingDir, "sachet-")
+		outfile, err := smst.createFile(recipient, message.Text)
 		if err != nil {
 			return err
 		}
 		defer outfile.Close()
 		outfile.WriteString("To: " + recipient + "\n")
+		outfile.WriteString("Alphabet: UTF-8\n")
 		outfile.WriteString("\n")
 		_, err = outfile.WriteString(message.Text)
 		if err != nil {
